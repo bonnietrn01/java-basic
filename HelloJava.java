1723a96e2fad7d85b5409da2a805c4243b322ce6
bonnietrn <pnhi279@gmail.com>
2024-06-19 16:19:43 +0900

    add HelloJava

---
+++ HelloJava.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class HelloJava {
+    public static void main(String[] args) {
+        // 출력
+        System.out.println("Hello World");
+
+        // 입력 받고 출력
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(scanner.nextLine());
+    }
+}
